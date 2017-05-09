13b74f0fb5f8b10e9ee159ffaf5ad0deb0876819
Gustavo <gustavo_nav@hotmail.com>
2017-05-09 00:08:16 -0300

    glicemia section

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -2,9 +2,34 @@ var app = angular.module('organize', []);
 
 app.controller('mainController', ['$scope', function($scope) {
   $scope.click = false;
-
   $scope.manipulateForm = function(boolean) {
     $scope.click = boolean;
-    console.log(boolean);
+    console.log('boolean');
   }
 }]);
+
+app.directive('glicemiaChart', function(){
+  return {
+    restrict: 'E',
+    link: function() {
+      var data = {
+        labels: ['7h', '10h', '13h', '16h', '19h', '22h'],
+        series: [
+          [110, 90, 95, 93, 55, 113]
+        ]
+      };
+
+      var options = {
+        height: 320,
+        lineSmooth: false,
+        onlyInteger: true,
+        fullWidth: true,
+        high: 200,
+        low: 0,
+
+      };
+
+      new Chartist.Line('.glicemia__chart', data, options);
+    }
+  }
+});
